c55661d925cf2793bb38ee53e30cae1d85bad1b3
jirawathra <jirawathr@gmail.com>
2023-01-12 14:05:46 +0700

    Add CustomerController

---
+++ app/Http/Controllers/V1/CustomerController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    //
+    public function index()
+    {
+        return "in customer controller";
+    }
+}
